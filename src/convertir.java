df4f4a358926a1d0b453591e3dbeb80f6f9608da
lady vanessa marulanda <marulandaladyvanessa@gmail.com>
2022-06-18 09:10:07 -0500

    software terminado

--- src/convertir.java
+++ src/convertir.java
@@ -1,4 +1,14 @@
+import javax.swing.*;
+import java.util.jar.JarOutputStream;
+
 public class convertir {
 
-    public sta
+    public static void main (String[]args){
+        double pesos = Double.parseDouble(JOptionPane.showInputDialog("por favor ingresar pesos"));
+        double bolivares = pesos*0.0014;
+        JOptionPane.showMessageDialog(null,"valor en bolivares es "+bolivares);
+
+
+
+    }
 }
